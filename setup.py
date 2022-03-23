9adbe4f0eb12a926fb6d0db319b5a857fdc74e1b
ybouteiller <yann.bouteiller@hotmail.fr>
2022-03-23 00:42:33 +0000

    online filtering

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(
                       'portilooplot',
                       'ipywidgets',
                       'python-periphery',
-                      'spidev',
-                      'scipy'
+                      'spidev'
                      ]
 )
