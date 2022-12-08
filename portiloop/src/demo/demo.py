8b07208d168bbf31185bc01b405537733a368486
Milo Sobral <milosobral@gmail.com>
2022-12-08 17:40:17 +0000

    Some minor changes

--- portiloop/src/demo/demo.py
+++ portiloop/src/demo/demo.py
@@ -11,13 +11,13 @@ def on_upload_file(file):
         yield f"File {file.name} successfully uploaded!"
 
 
-with gr.Blocks() as demo:
+with gr.Blocks(title="Portiloop") as demo:
     gr.Markdown("# Portiloop Demo")
-    gr.Markdown("This Demo takes as input a csv file containing EEG data and outputs a csv file with the following added: \n * The data filtered by the Portiloop online filter \n * The stimulations made by Portiloop.")
+    gr.Markdown("This Demo takes as input an XDF file coming from the Portiloop EEG device and allows you to convert it to CSV and perform the following actions:: \n * Filter the data offline \n * Perform offline spindle detection using Wamsley or Lacourse. \n * Simulate the Portiloop online filtering and spindle detection with different parameters.")
     gr.Markdown("Upload your CSV file and click **Run Inference** to start the processing...")
 
     with gr.Row():
-        xdf_file = gr.UploadButton(label="Upload XDF File", type="file")
+        xdf_file = gr.UploadButton(label="XDF File", type="file")
 
         # Offline Filtering (Boolean)
         offline_filtering = gr.Checkbox(label="Offline Filtering (On/Off)", value=True)
@@ -38,17 +38,24 @@ with gr.Blocks() as demo:
         freq = gr.Dropdown(choices=["100", "200", "250", "256", "500", "512", "1000", "1024"], value="250", label="Sampling Frequency (Hz)", interactive=True)
 
     # Output elements
-    update_text = gr.Textbox(value="Waiting for user input...", label="Status", interactive=False)
+    update_text = gr.Textbox(value="Upload an XDF File and click 'Run Inference'...", label="Status", interactive=False)
     output_plot = gr.Plot()
     output_array = gr.File(label="Output CSV File")
     xdf_file.upload(fn=on_upload_file, inputs=[xdf_file], outputs=[update_text])
 
+    def clear():
+        output_plot.clear()
+        output_array.clear()
+        update_text.clear()
+        xdf_file.clear()
+
     # Row containing all buttons:
     with gr.Row():
         # Run inference button
-        run_inference = gr.Button(value="Run Inference")
+        run_inference = gr.Button(
+            value="Run Inference")
         # Reset button
-        reset = gr.Button(value="Reset", variant="secondary")
+        reset = gr.Button(value="Reset", variant="secondary", on_click=clear,)
     run_inference.click(
         fn=run_offline, 
         inputs=[
@@ -64,4 +71,4 @@ with gr.Blocks() as demo:
         outputs=[output_plot, output_array, update_text])
 
 demo.queue()
-demo.launch(share=True)
+demo.launch(share=False)
