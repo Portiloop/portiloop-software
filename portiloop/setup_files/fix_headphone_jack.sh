c5bab3f901aa1a545130360177baacd29a43cd11
Yann Bouteiller <yann.bouteiller@hotmail.fr>
2026-07-18 02:50:59 -0400

    test

--- portiloop/setup_files/fix_headphone_jack.sh
+++ portiloop/setup_files/fix_headphone_jack.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sleep 2
+
 JACK_STATE=$(amixer -c 0 cget iface=CARD,name='Headphone Jack' | grep -o 'values=on\|values=off')
 if [ "$JACK_STATE" = "values=on" ]; then
     amixer -c 0 cset iface=MIXER,name='Headphone Switch' on
@@ -7,8 +8,6 @@ else
     amixer -c 0 cset iface=MIXER,name='Headphone Switch' off
 fi
 
-# Briefly open a capture stream in the background to prime the shared
-# ADDA analog front end, then run playback
-timeout 2 arecord -D hw:0,0 -f S16_LE -r 48000 -c 2 /dev/null &
+timeout 2 arecord -D hw:0,1 -f S16_LE -r 48000 -c 2 /dev/null &
 speaker-test -t wav -c 2 -D plughw:0,0 -l 1
 wait
