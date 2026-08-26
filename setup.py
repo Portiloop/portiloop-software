275e657ad4d28407ae81cdc9c47f6b8467cb885e
Yann Bouteiller <yann.bouteiller@hotmail.fr>
2026-08-26 17:34:14 -0400

    release 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import io
 
 
-VERSION = '0.1.3'  # Note: don't forget to update this in the top __init__.py
+VERSION = '0.1.4'  # Note: don't forget to update this in the top __init__.py
 
 
 def is_coral():
