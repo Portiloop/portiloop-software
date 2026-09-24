eec8a9f2701321c4fb5bc2da7462c46515024917
Yann Bouteiller <yann.bouteiller@hotmail.fr>
2026-09-24 16:46:55 -0400

    logging

--- portiloop/__init__.py
+++ portiloop/__init__.py
@@ -1,1 +1,19 @@
+import logging
+import sys
+from pathlib import Path
+
+
 __version__ = '0.1.4'
+
+
+LOG_FOLDER = Path.home() / 'workspace' / 'logs'
+LOG_FOLDER.mkdir(parents=True, exist_ok=True)
+LOG_FILE = LOG_FOLDER / 'portiloop.log'
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(processName)s %(name)s (%(filename)s:%(lineno)d): %(message)s',
+    handlers=[
+        logging.FileHandler(LOG_FILE),
+        logging.StreamHandler(sys.stdout),
+    ],
+)
